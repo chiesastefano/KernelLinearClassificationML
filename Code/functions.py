b35006172d179c16284ce718fca786e2deb40456
Stefano Chiesa <146427197+chiesastefano@users.noreply.github.com>
2024-08-03 16:56:55 +0200

    Perceptron Algorithm
    Still have to run a test

--- Code/functions.py
+++ Code/functions.py
@@ -1,16 +1,32 @@
+
 import numpy as np
+def perceptron(x, y, n, epochs=1):
+    """
+    Train a Perceptron classifier using the Perceptron learning algorithm.
 
-def perceptron(x, y, n):
-    w = np.zeros(x.shape[1] + 1)    # Initialize the weights + bias term
-    x = np.insert(x, 0, 1, axis=1)  # Add the bias term to the input data
-    for i in range(x.shape[0]):
-        prediction = np.dot(w, x[i])
-        # Perceptron update rule
-        if prediction <= 0 and y[i] == 1:
-            w = w + n * x[i]
-        elif prediction > 0 and y[i] == -1:
-            w = w - n * x[i]
+    Parameters:
+    ----------
+    x : The input feature matrix
+    y : The target labels that contain values -1 or 1.
+    n : The learning rate for weight updates.
+    epochs (optional): The number of times to iterate over the entire dataset during training.
+
+    Returns:
+    -------
+    w = The final weights of the Perceptron after training, including the bias term.
+    """
+    x = np.insert(x, 0, 1, axis=1)  # Insert a column of ones for the bias term
+    w = np.zeros(x.shape[1])  # Initialize weights to zero
+
+    for epoch in range(epochs):
+        for i in range(x.shape[0]):
+            prediction = np.dot(w, x[i])
+            # Perceptron update rule
+            if prediction <= 0 and y[i] == 1:
+                w = w + n * x[i]
+            elif prediction > 0 and y[i] == -1:
+                w = w - n * x[i]
+
     return w
-
 
 
