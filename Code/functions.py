57a3efb6db8d6689e6149a1e74f6a0f3f8a11826
Stefano Chiesa <146427197+chiesastefano@users.noreply.github.com>
2024-08-05 16:54:30 +0200

    Pegasos
    Done

--- Code/functions.py
+++ Code/functions.py
@@ -1,12 +1,12 @@
 
 import numpy as np
-def perceptron(x, y, n, epochs=1):
+def perceptron(x, y, n, epochs=1000):
     """
     Train a Perceptron classifier using the Perceptron learning algorithm.
 
     Parameters:
     ----------
-    x : The input feature matrix
+    x : The input feature matrix.
     y : The target labels that contain values -1 or 1.
     n : The learning rate for weight updates.
     epochs (optional): The number of times to iterate over the entire dataset during training.
@@ -44,3 +44,53 @@ def zero_one_loss(y_pred, y_true):
     """
     misclassifications = np.sum(y_pred != y_true)
     return int(misclassifications)
+
+
+def pegasos(x, y, lam, epochs, batch_size):
+    """
+    Train a Support Vector Machine (SVM) classifier using the Pegasos algorithm.
+
+    Parameters:
+    ----------
+    x : The input feature matrix.
+    y : The target labels that contain values -1 or 1.
+    lam : The regularization parameter that controls the trade-off between maximizing the margin and minimizing the classification error.
+    epochs : The number of times to iterate over the entire dataset during training.
+    batch_size : The number of samples to process in each batch update step.
+
+    Returns:
+    -------
+    w : The final weights of the SVM after training.
+    predictions = The predicted labels for the input data.
+    """
+
+    if batch_size > x.shape[0]:
+        return "Batch size cannot be larger than the number of samples in the dataset."
+
+    # Initialize weight vector with zeros
+    w = np.zeros(x.shape[1])
+    t = 0  # Iteration counter
+    w_sum = np.zeros_like(w)  # Initialize weight sum
+    for epoch in range(epochs):
+        for i in range(x.shape[0] // batch_size):
+            t += 1
+            # Learning rate as a function of the iteration count
+            eta = 1 / (lam * t)
+            # Randomly sample a batch of indices
+            batch_indices = np.random.randint(0, x.shape[0], batch_size)
+            x_batch = x[batch_indices]
+            y_batch = y[batch_indices]
+
+            # Update weights based on the current batch
+            for j in range(batch_size):
+                if y_batch[j] * np.dot(w, x_batch[j]) < 1:
+                    w = (1 - eta * lam) * w + eta * y_batch[j] * x_batch[j]
+                else:
+                    w = (1 - eta * lam) * w
+
+            w_sum += w
+
+    w = w_sum/t
+    predictions = np.sign(np.dot(x, w))
+
+    return w, predictions
